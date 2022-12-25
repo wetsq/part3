17e91b87e80c9b09e60af7fcbf37ff6f62194f15
Veeti <veeti.salminen@tuni.fi>
2022-12-25 20:02:34 +0200

    3.11

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const morgan = require('morgan')
+const cors = require('cors')
 
 morgan.token('data', (request) => {
     return JSON.stringify(request.body)
@@ -7,6 +8,8 @@ morgan.token('data', (request) => {
 
 const app = express()
 
+app.use(express.static('build'))
+app.use(cors())
 app.use(express.json())
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :data'))
 
@@ -80,7 +83,7 @@ app.post('/api/persons', (request, response) => {
     response.json(person)
 })
 
-const PORT = 3001
+const PORT = process.env.PORT || 8080
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
 })
